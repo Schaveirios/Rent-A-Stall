c24a5e2f6dd27f38ae29f294991782b520b2ba9c
JomaryDaison16 <jomari.daison@gmail.com>
2017-11-08 11:56:47 +0800

    add photo

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,8 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, validators, SubmitField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+
 
 class Tenants(Form):
 
@@ -9,6 +12,7 @@ class Tenants(Form):
     Address = StringField('Present Address', [validators.Length(min=10,max=50)])
     ContNum = StringField('Contact Number', [validators.Length(min=10,max=50)])
     stallno = StringField('Stall Number',[validators.DataRequired(),validators.Length(min=6,max=20)])
+    TenantphotoID = FileField('TenantphotoID')
     submit = SubmitField("Submit")
 
 
@@ -17,5 +21,17 @@ class Stalls(Form):
 	stallno = StringField('stallno',[validators.DataRequired(),validators.Length(min=6,max=20)])
 	stallloc = StringField('stallloc',[validators.DataRequired(),validators.Length(min=6,max=20)])
     rate = StringField('rate',[validators.DataRequired(),validators.Length(min=6,max=20)])
-    
+    location = StringField('location',[validators.DataRequired(),validators.Length(min=6,max=20)])
+
+    submit = SubmitField("Submit")
+
+class User(Form):
+
+    uname = StringField('Username',[validators.DataRequired(),validators.Length(min=6,max=20)])
+    fname = StringField('Firstname',[validators.DataRequired(),validators.Length(min=6,max=20)])
+    mname = StringField('Midlename',[validators.DataRequired(),validators.Length(min=6,max=20)])
+    lname = StringField('Lastname',[validators.DataRequired(),validators.Length(min=6,max=20)])
+    password = StringField('Password', [validators.Length(min=10,max=50)])
+    ContNum = StringField('Contact Number', [validators.Length(min=10,max=50)])
+        
     submit = SubmitField("Submit")
