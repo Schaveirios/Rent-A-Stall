16a753558303af8d55fd335270abd4572688ac5e
JomaryDaison16 <jomari.daison@gmail.com>
2017-11-08 16:11:00 +0800

    modified

--- app/forms.py
+++ app/forms.py
@@ -6,14 +6,9 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 class Tenants(Form):
 
-<<<<<<< HEAD
-	sname = StringField('Surename',[validators.DataRequired(),validators.Length(min=6,max=20)])
-	mname = StringField('Middlename',[validators.DataRequired(),validators.Length(min=6,max=20)])
-=======
 	fname = StringField('Firstname',[validators.DataRequired(),validators.Length(min=6,max=20)])
-	mname = StringField('Midlename',[validators.DataRequired(),validators.Length(min=6,max=20)])
->>>>>>> 03df3a4ce5d3baea6977eb69484479da36d01e4b
-    lname = StringField('Lastname',[validators.DataRequired(),validators.Length(min=6,max=20)])
+	mname = StringField('Middlename',[validators.DataRequired(),validators.Length(min=6,max=20)])
+	lname = StringField('Lastname',[validators.DataRequired(),validators.Length(min=6,max=20)])
     Address = StringField('Present Address', [validators.Length(min=10,max=50)])
     ContNum = StringField('Contact Number', [validators.Length(min=10,max=50)])
     stallno = StringField('Stall Number',[validators.DataRequired(),validators.Length(min=6,max=20)])
@@ -34,7 +29,7 @@ class User(Form):
 
     uname = StringField('Username',[validators.DataRequired(),validators.Length(min=6,max=20)])
     fname = StringField('Firstname',[validators.DataRequired(),validators.Length(min=6,max=20)])
-    mname = StringField('Midlename',[validators.DataRequired(),validators.Length(min=6,max=20)])
+    mname = StringField('Middlename',[validators.DataRequired(),validators.Length(min=6,max=20)])
     lname = StringField('Lastname',[validators.DataRequired(),validators.Length(min=6,max=20)])
     password = StringField('Password', [validators.Length(min=10,max=50)])
     ContNum = StringField('Contact Number', [validators.Length(min=10,max=50)])
