0a56c485eead51b98e36e12acd3d15f0bab416f1
Schaveirios <mcprivateer16@gmail.com>
2017-11-14 15:52:19 +0800

    Update on models, forms (log in/sign up)

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import DB_USERNAME, DB_HOST, DB_NAME, DB_PASSWORD
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 dbase = SQLAlchemy(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/dbase'
-
+app.config['SECRET_KEY'] = "Password"
 import models
+Bootstrap(app)
 
 dbase.create_all()
 app.debug = True
+
+from app import controller
--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from app import dbase
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Branch(dbase.Model):
     __tablename__ = "branch"
@@ -14,32 +14,45 @@ class Branch(dbase.Model):
     def __repr__(self):
         return '<branch_loc {}>'.format(self.branch_loc)
 
+    @staticmethod
+    def branch_types():
+        brLoc = ['Palao', 'Tambo', 'Central Market']
+        for b in brLoc:
+            loc = Branch.query.filter_by(branch_loc = b).first()
+            if loc is None:
+                loc = Branch(branch_loc= b, branch_logo='Default')
+            dbase.session.add(loc)
+        dbase.session.commit()
+
+
 class Stalls(dbase.Model):
     __tablename__ = 'stalls'
     stallID = dbase.Column(dbase.Integer, primary_key=True, autoincrement= True)
     stall_no = dbase.Column(dbase.Integer)
-    stall_rate = dbase.Column(dbase.Float)
+    stall_rate = dbase.Column(dbase.Integer)
     stall_loc = dbase.Column(dbase.String(60))
     stall_status = dbase.Column(dbase.String(60))
     typeID = dbase.Column(dbase.Integer, dbase.ForeignKey("types.typeID"), nullable= False)
     branchID = dbase.Column(dbase.Integer, dbase.ForeignKey("branch.branchID"), nullable=False)
 
-    def __init__ (self,stall_rate, stall_loc, stall_status, branchID, stall_no):
+    def __init__ (self,stall_rate, stall_loc, stall_status, branchID, stall_no, typeID):
         self.stall_rate =stall_rate
         self.stall_status = '0'
         self.stall_loc = stall_loc
         self.stall_no = stall_no
         self.branchID = branchID
+        self.typeID = typeID
 
     def __repr__(self):
         return '<stall_rate{}>'.format(self.stall_rate)
+
 
 class Users(dbase.Model):
     __tablename__ ="users"
     userID = dbase.Column(dbase.Integer,primary_key= True, autoincrement= True)
     roleID = dbase.Column(dbase.Integer)
     username = dbase.Column(dbase.String(20), unique=True)
-    passwrd = dbase.Column(dbase.String(20))
+    passwrd = dbase.Column(dbase.String(255))
     contact_no = dbase.Column(dbase.String(12))
     first_name = dbase.Column(dbase.String(24))
     mid_name = dbase.Column(dbase.String(24))
@@ -49,13 +62,13 @@ class Users(dbase.Model):
     def __init__(self, roleID, username,passwrd,contact_no,first_name,mid_name,last_name,branchID):
         self.roleID = roleID
         self.username = username
-        self.passwrd = passwrd
+        self.passwrd = generate_password_hash(passwrd)
         self.first_name = first_name
         self.mid_name= mid_name
         self.last_name= last_name
         self.contact_no = contact_no
         self.branchID = branchID
-
+        super(Users, self).__init__()
     def __repr__(self):
         return '<roleID {}>'.format(self.roleID)
 
@@ -78,6 +91,7 @@ class Tenants(dbase.Model):
         self.present_addr = present_addr
         self.tenant_photo = tenant_photo
         self.stallID = stallID
+
 
     def __repr__(self):
         return '<last_name {}>'.format(self.last_name)
@@ -149,3 +163,12 @@ class Types(dbase.Model):
     def __repr__(self):
         return '<stall_type{}>'.format(self.stall_type)
 
+    @staticmethod
+    def stall_types():
+        stypes = ['Carenderia', 'Stall2', 'Stall3']
+        for s in stypes:
+            tyype = Types.query.filter_by(stall_type = s).first()
+            if tyype is None:
+                tyype = Types(stall_type= s)
+            dbase.session.add(tyype)
+        dbase.session.commit()
