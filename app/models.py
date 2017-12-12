5df4509daff561c6d6de34c77d620e3b1e469b28
Eduardo Quiobe <31158420+Schaveirios@users.noreply.github.com>
2017-12-12 16:40:25 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from app import app
 from flask_login import AnonymousUserMixin
 from sqlalchemy.orm import relationship
 from sqlalchemy import Integer, ForeignKey, String, Column
+from datetime import datetime
 
 class Branch(dbase.Model):
     __tablename__ = "branch"
@@ -21,7 +22,7 @@ class Branch(dbase.Model):
 
     @staticmethod
     def branch_types():
-        brLoc = ['Palao', 'Tambo', 'Central Market']
+        brLoc = ['Palao']
         for b in brLoc:
             loc = Branch.query.filter_by(branch_loc = b).first()
             if loc is None:
@@ -38,19 +39,19 @@ class Stalls(dbase.Model):
     stall_loc = dbase.Column(dbase.String(60))
     stall_status = dbase.Column(dbase.String(60))
     typeID = dbase.Column(dbase.Integer, dbase.ForeignKey("types.typeID"), nullable= False)
-    branchID = dbase.Column(dbase.Integer, dbase.ForeignKey("branch.branchID"), nullable=False)
+    # branchID = dbase.Column(dbase.Integer, dbase.ForeignKey("branch.branchID"), nullable=False)
     t_stall_no = relationship("Tenants", backref="stallNO", lazy="dynamic" )
-    def __init__ (self,stall_rate, stall_loc, stall_status, branchID, stall_no, typeID):
+    def __init__ (self,stall_rate, stall_loc, stall_status, stall_no, typeID):
         self.stall_rate =stall_rate
         self.stall_status = '0'
         self.stall_loc = stall_loc
         self.stall_no = stall_no
-        self.branchID = branchID
+        # self.branchID = branchID
         self.typeID = typeID
 
     def __repr__(self):
         return '<stall_rate{}>'.format(self.stall_rate)
-
+        
 
 class Users(dbase.Model):
     __tablename__ ="users"
@@ -62,9 +63,9 @@ class Users(dbase.Model):
     first_name = dbase.Column(dbase.String(24))
     mid_name = dbase.Column(dbase.String(24))
     last_name = dbase.Column(dbase.String(24))
-    branchID = dbase.Column(dbase.Integer, dbase.ForeignKey("branch.branchID"), nullable=False)
+    # branchID = dbase.Column(dbase.Integer, dbase.ForeignKey("branch.branchID"), nullable=False)
 
-    def __init__(self, roleID, username,passwrd,contact_no,first_name,mid_name,last_name,branchID):
+    def __init__(self, roleID, username,passwrd,contact_no,first_name,mid_name,last_name):
         self.roleID = roleID
         self.username = username
         self.passwrd = generate_password_hash(passwrd)
@@ -72,7 +73,7 @@ class Users(dbase.Model):
         self.mid_name= mid_name
         self.last_name= last_name
         self.contact_no = contact_no
-        self.branchID = branchID
+        # self.branchID = branchID
 
 
     def is_authenticated(self):
@@ -131,13 +132,12 @@ class Tenants(dbase.Model):
     tenant_photo = dbase.Column(dbase.String(60))
     stallID = dbase.Column(dbase.Integer, dbase.ForeignKey("stalls.stallID"), nullable=True)
 
-    def __init__(self,contact_no, first_name, mid_name, last_name, present_addr, tenant_photo, stallID):
+    def __init__(self,contact_no, first_name, mid_name, last_name, present_addr, stallID):
         self.first_name = first_name
         self.mid_name = mid_name
         self.last_name = last_name
         self.contact_no = contact_no
         self.present_addr = present_addr
-        self.tenant_photo = tenant_photo
         self.stallID = stallID
 
 
@@ -148,16 +148,14 @@ class Logs(dbase.Model):
     __tablename__ = "logs"
     logID = dbase.Column(dbase.Integer, primary_key=True, autoincrement=True)
     details = dbase.Column(dbase.String(60))
-    log_date =dbase.Column(dbase.DATE)
-    branchID = dbase.Column(dbase.Integer,dbase.ForeignKey("branch.branchID"), nullable=True)
+    log_date =dbase.Column(dbase.DateTime)
 
-    def __init__(self, details, log_date, branchID):
+    def __init__(self, details, log_date):
         self.details = details
         self.log_date = log_date
-        self.branchID = branchID
 
     def __repr__(self):
-        return '<details{}>'.format(self.branchID)
+        return '<details{}>'.format(self.logID)
 
 
 class Pays(dbase.Model):
@@ -166,19 +164,26 @@ class Pays(dbase.Model):
     issued_by = dbase.Column(dbase.String(60))
     reference_no = dbase.Column(dbase.String(60))
     or_no = dbase.Column(dbase.String(60))
-    rent = dbase.Column(dbase.Float)
+    # rent = dbase.Column(dbase.Float)
     sCharge = dbase.Column(dbase.Float)
+    month = dbase.Column(dbase.String(20))
+    amount = dbase.Column(dbase.Float)
+    total = dbase.Column(dbase.Float)
+    date_issued =dbase.Column(dbase.DATE)
     tenantID = dbase.Column(dbase.Integer,dbase.ForeignKey("tenants.tenantID"), nullable=True)
     stallID = dbase.Column(dbase.Integer, dbase.ForeignKey("stalls.stallID"), nullable=True)
 
-    def __init__(self, issued_by, reference_no,or_no,rent,sCharge, tenantID, stalID):
+    def __init__(self, issued_by,or_no,sCharge, tenantID, stallID,month, amount,total,date_issued):
         self.issued_by = issued_by
         self.or_no = or_no
-        self.rent = rent
+        # self.rent = rent
         self.sCharge = sCharge
         self.tenantID = tenantID
-        self.stallID = stalID
-
+        self.stallID = stallID
+        self.month = month
+        self.amount = amount
+        self.total = total
+        self.date_issued = date_issued
 
     def __repr__(self):
         return '< issued_by{}>'.format(self.issued_by)
