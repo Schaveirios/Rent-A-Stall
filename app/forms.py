5b8a9349c377fa8a94c2ed3ec63782d3ad4ac74d
Eduardo Quiobe <31158420+Schaveirios@users.noreply.github.com>
2017-12-12 16:41:11 +0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, validators, SubmitField, SelectField, IntegerField
+from wtforms import StringField, PasswordField, validators, SubmitField, SelectField, IntegerField, DateField
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms.validators import InputRequired,Length
 
@@ -15,19 +15,19 @@ class addtenants(Form):
     stallno = StringField('Stall Number',[validators.DataRequired(),validators.Length(min=1,max=20)])
     tenantphotoID = FileField('TenantphotoID')
     stallloc = StringField('Stall type',[validators.DataRequired(),validators.Length(max=20)])
-    branch = SelectField(u'branches', choices=[('1', 'Tambo'), ('2', 'Pala-o'), ('3', 'wet market')])
+    datestarted = DateField('Date Started', format="%m/%d/%Y")
+    #branch = StringField(u'branches',[validators.DataRequired(),validators.Length(max=10)])
     submit = SubmitField("Submit")
+    # status = RadioField('Approval Status',[validators.DataRequired()],choices=[('1','Approved'),('0','Pending')])
 
 
 
 class addstalls(Form):
 
-    stallno = IntegerField('stallno',[validators.DataRequired(),validators.Length(max=20)])
+    stallno = StringField('stallno',[validators.DataRequired(),validators.Length(max=20)])
     stallloc = StringField('stallloc',[validators.DataRequired(),validators.Length(max=20)])
-    rate = IntegerField('rate',[validators.DataRequired(),validators.Length(max=20)])
-    Branchloc = StringField('Stall type',[validators.DataRequired(),validators.Length(max=20)])
+    rate = StringField('rate',[validators.DataRequired(),validators.length(max=20)])
     stalltype = StringField('Stall type',[validators.DataRequired(),validators.Length(max=20)])
-
     submit = SubmitField("Add")
 
 # class addclerk(Form):
@@ -41,11 +41,11 @@ class addstalls(Form):
 #     branchId = SelectField(u'branches', choices=[('1', 'Tambo'), ('2', 'Pala-o'), ('3', 'wet market')])
 #     submit = SubmitField("Add")
 
-class addbranch(Form):
+# class addbranch(Form):
 
-    location = StringField('location',[validators.DataRequired(),validators.Length(min=6,max=20)])
+#     location = StringField('location',[validators.DataRequired(),validators.Length(min=6,max=20)])
 
-    submit = SubmitField("Submit")
+#     submit = SubmitField("Submit")
 
 class LogIn(Form):
 
@@ -61,4 +61,26 @@ class RegisterForm(Form):
     mname =  StringField('Middle Name', validators=[InputRequired(), Length(min=4, max=15)])
     lname =  StringField('Last Name', validators=[InputRequired(), Length(min=2, max=15)])
     ContNum =  StringField('Contact Number', validators=[InputRequired(), Length(min=11, max=15)])
-    branchID = SelectField(u'branches', choices=[('1', 'Tambo'), ('2', 'Palao'), ('3', 'Central Market')])
+    # branchID = StringField('branches',[validators.DataRequired(),validators.Length(max=20)])
+    submit = SubmitField("Add")
+	
+class PaymentForm(Form):
+    month = StringField('Month', validators=[InputRequired(), Length(min=1, max=64)])
+    amount = StringField('Amount', validators=[InputRequired(), Length(min=1, max=64)])
+    sCharge = StringField('S. Charge', validators=[InputRequired(), Length(min=1, max=64)])
+    total = StringField('Total', validators=[InputRequired(), Length(min=1, max=64)])
+    or_no = StringField('OR Num', validators=[InputRequired(), Length(min=1, max=64)])
+    date_issued = StringField('Date Issued', validators=[InputRequired(), Length(min=3, max=64)])
+    issued_by = StringField('Issued By', validators=[InputRequired(), Length(min=4, max=64)])
+    submit = SubmitField("Submit")
+
+
+class edit_tenant_form(Form):
+
+    fname = StringField('Firstname',[validators.DataRequired(),validators.Length(min=2,max=20)])
+    mname = StringField('Middlename',[validators.DataRequired(),validators.Length(min=2,max=20)])
+    lname = StringField('Lastname',[validators.DataRequired(),validators.Length(min=2,max=20)])
+    address = StringField('Present Address', [validators.Length(min=5,max=50)])
+    contnum = StringField('Contact Number', [validators.Length(min=11,max=11)])
+    tenantphotoID = FileField('TenantphotoID')
+    submit = SubmitField("Submit")
